63f1c3a02709d1d68f7b4a30de3d9a666e9c03b7
jiga <manjumusei@gmail.com>
2026-05-02 00:27:22 +0800

    feat: implement HTTP server with ClientHandler for serving client files

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-hello world
+import asyncio
+import os
+import sys
+import threading
+from http.server import SimpleHTTPRequestHandler, ThreadingHTTPServer
+
+import websockets
+
+from server.game import GameServer
+class ClientHandler(SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory=os.path.join(os.path.dirname(__file__), "client"), **kwargs)
+
+    def log_message(self, format, *args):
+        pass # Supress noise for now so i can troubleshoot it easier later
+
+
+def start_http_server(host: str = "127.0.0.1", port: int = 8080):
+    server= ThreadingHTTPServer((host, port), ClientHandler)
+    thread = threading.Thread(target=server.serve_forever, daemon=True)
+    thread.start()
+    print(f"[HTTP] Client served at http://{host}:{port}/")
+    return server
+
+
