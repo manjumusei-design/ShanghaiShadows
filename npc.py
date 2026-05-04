17a71b7fb1277943638a3a6319c59002033ecf08
jiga <manjumusei@gmail.com>
2026-05-05 01:11:50 +0800

    wrote load npcs function + npc start

--- npc.py
+++ npc.py
@@ -15,4 +15,17 @@ class Npc:
     memory: List[str] = field(default_factory=list)
 
 
-    
+def load_npcs(path: str) -> Dict[str, Npc]:
+    with open(path, "r", encoding="utf-8") as f:
+        data = yaml.safe_load(f)
+    npcs = {}
+    for npc_data in data.get("npcs", []):
+        npcs[npc_data["id"]] = Npc(
+            id=npc_data["id"],
+            name=npc_data["name"],
+            description=npc_data["description"],
+            faction=npc_data["faction"],
+            schedule=npc_data.get("schedule", {}),
+            dialogue=npc_data.get("dialogue",{})
+        )
+    return npcs
