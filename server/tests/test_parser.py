48b3fa7b7b422a77432bbcba90b40d5a997da4ca
Jiga <manjumusei@gmail.com>
2026-05-18 15:58:44 +0800

    Add parser tests for basic verbs

--- server/tests/test_parser.py
+++ server/tests/test_parser.py
@@ -15,4 +15,25 @@ class TestTokenizer(unittest.TestCase):
     def test_unbalanced_quotes_fallback(self):
         self.assertEqual(tokenize('take "key'), ["take", '"key'])
 
-        
+
+class TestParserSimpleVerbs(unittest.TestCase):
+    def test_empty(self):
+        cmd = parse("")
+        self.assertEqual(cmd.verb, "pass")
+
+    def test_look(self):
+        cmd = parse("look")
+        self.assertEqual(cmd.verb, "look")
+        self.assertIsNone(cmd.direct_obj)
+
+    def test_look_alias_l(self):
+        cmd = parse("l")
+        self.assertEqual(cmd.verb, "look")
+
+    def test_inventory(self):
+        cmd = parse("inventory")
+        self.assertEqual(cmd.verb, "inventory")
+
+    def test_inventory_alias_i(self):
+        cmd = parse("i")
+        self.assertEqual(cmd.verb, "inventory")
