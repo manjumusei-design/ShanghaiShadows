14b0364e0a5c19d9f05234394a5b2f7aeb3b74f2
jiga <manjumusei@gmail.com>
2026-06-01 00:11:40 +0800

    Update accent color in index.html and add constants for game configuration

---
+++ server/constants.py
@@ -0,0 +1,16 @@
+CURFEW_MINUTE = 1260
+EVENT_LOG_MAXLEN = 500
+WORLD_EVENTS_MAXLEN = 50
+CONVERSATION_HISTORY_MAXLEN = 20
+NPC_MEMORY_MAXLEN = 30
+HUNGER_DECAY_RATE = 0.5
+HUNGER_HEALTH_DAMAGE = 2
+LOW_HUNGER_THRESHOLD = 20
+STATE_BROADCAST_INTERVAL = 5
+
+EVENTS_PATH = "server/data/events.yaml"
+TRUST_RULES_PATH = "server/data/trust_rules.yaml"
+DISGUISES_PATH = "server/data/disguises.yaml"
+STORYLETS_PATH = "server/data/storylets.yaml"
+OBITUARY_PATH = "server/data/obituary_templates.yaml"
+BACKGROUNDS_PATH = "server/data/character)backgrounds.yaml"
